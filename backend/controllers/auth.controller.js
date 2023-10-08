912ea5800681976337564cda83987365d763e498
Americano.Dev <andrenations@gmail.com>
2023-10-08 19:15:38 -0400

    created signup route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const saltRounds = 10;
+  const hashedPassword =  bcryptjs.hashSync(password, saltRounds);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -18,3 +19,5 @@ app.use(express.json());
 app.listen(3001, () => {
     console.log('Server Listening on port 3001!')
 })
+
+app.use('/backend/auth', authRoutes);
