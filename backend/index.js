8965174a17a25aa85e569388edc9a09bed8dc9bb
Americano.Dev <andrenations@gmail.com>
2023-10-07 22:48:56 -0400

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
 import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-
-
-
-
-
-
-
+mongoose.connect(process.env.MONGO)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const app = express();
 
 app.use(express.json());
+ 
 
 app.listen(3001, () => {
-    console.log('Server Listening on port 3000')
+    console.log('Server Listening on port 3001!')
 })
