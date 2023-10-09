7203c8a09bca8051216caa4ee2bb3a156bfda1d5
Americano.Dev <andrenations@gmail.com>
2023-10-09 16:20:59 -0400

    created sign up page

--- frontend/src/screens /SignUp.jsx
+++ frontend/src/screens /SignUp.jsx
@@ -1,10 +1,75 @@
-import Container from 'react-bootstrap/Container';
+import { Form, Button, Container, Col } from 'react-bootstrap';
 
-export default function LogIn() {
-  return (
-    <Container className="ml-auto">
-      {/* Your SignIn content goes here */}
-      <div>Sign Up</div>
-    </Container>
-  );
-}
+export default function SignUp() {
+  
+    return (
+      <Container className="d-flex justify-content-center align-items-center">
+        <div className="bg-white p-4">
+        <h2 className="text-2xl text-center font-semibold mb-4 text-dark">Sign Up</h2>
+  
+          <Form>
+            <Form.Group controlId="username">
+            <Col xs={2}>
+              <Form.Label>Username</Form.Label>
+            </Col>
+              <Form.Control
+                type="text"
+                name="username"
+                
+                className="w-full px-5 py-2 border rounded-lg"
+                
+              />
+            </Form.Group>
+  
+            <Form.Group controlId="email">
+            <Col xs={2}>
+              <Form.Label className="mt-2">Email</Form.Label>
+            </Col>
+              
+              <Form.Control
+                type="email"
+                name="email"
+                
+                className="w-full px-5 py-2 border rounded-lg"
+                
+              />
+            </Form.Group>
+  
+            <Form.Group controlId="password">
+             <Col xs={2}>
+              <Form.Label className="mt-2" >Password</Form.Label>
+            </Col>
+              <Form.Control
+                type="password"
+                name="password"
+                
+                className="w-full px-5 py-2 border rounded-lg"
+               
+              />
+            </Form.Group>
+  
+            <Button variant="primary" type="button"   className='w-100 --btn mt-3' 
+                href="/" style={{ color: 'gold', border: 'none' }}>Sign Up</Button>
+          </Form>
+  
+          <p className="text-gray-600 text-sm mt-4">
+            Have an account?{' '}
+            <a href="/log-in" className="text-green-500">
+              Log in
+            </a>
+          </p>
+          {/* <p className="text-red-700 mt-5">{error && 'Password does not match!'}</p> */}
+        </div>
+      </Container>
+    );
+  }
+
+
+
+
+
+
+
+  
+
+
