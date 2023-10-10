475d485fab6267c2eddd4b0725d4261c065f2d2e
Americano.Dev <andrenations@gmail.com>
2023-10-10 17:45:32 -0400

    changed the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,45 +1,69 @@
-import { useState } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar } from "react-bootstrap";
+import { motion } from 'framer-motion';
 
 const AppNavbar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
-
   return (
-    <Navbar bg="warning" expand="lg">
-      <Container>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-white fixed-top">
+      <div className="container">
         <Navbar.Brand href="/products" className="mr-auto" 
-        style={{ color: 'white', fontSize: '23px', fontFamily: 'Times New Roman', fontWeight: 'bolder' }}>
-          AvaloZe
+          style={{ color: 'black', fontSize: '23px', fontFamily: 'Times New Roman', fontWeight: 'bolder' }}>
+          AVaLoZe
         </Navbar.Brand>
 
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          onClick={toggleNav} 
+        <button
+          className="navbar-toggler shadow-none border-0"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
         >
-          {isNavOpen ? 'X' : <div>&#9776;</div>}
-        </Navbar.Toggle>
-
-        <Navbar.Collapse id="basic-navbar-nav" className={`flex-column ${isNavOpen ? 'show' : ''}`}>
-          <Nav className="ml-auto">
-            <Nav.Link href="/products" style={{fontWeight: 'bolder'}}>Products</Nav.Link>
-            <Nav.Link type="button" className="--btn" href="/log-in" style={{ color: 'white' }}>
-              Log In
-            </Nav.Link>
-            <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-
-        <Nav className="ml-auto">
-          <Nav.Link href="#cart" style={{ color: 'grey', fontSize: '20px' }}>
-            🛒Cart
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="offcanvas offcanvas-top"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          style={{ height: '60vh', color: 'black' }}
+        >
+          <div className="offcanvas-header">
+            <Navbar.Brand href="/products" className="me-5" 
+              style={{ color: 'black', fontSize: '23px', fontFamily: 'Times New Roman', fontWeight: 'bolder' }}>
+              AVaLoZe
+            </Navbar.Brand>
+            <button
+              type="button"
+              className="btn-close shadow-none border-0"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1">
+              <motion.li className="nav-item" whileHover={{ scale: 1.2 }}>
+                <a className="nav-link active" aria-current="page" href="/products">
+                  Products
+                </a>
+              </motion.li>
+              <motion.li className="nav-item" whileHover={{ scale: 1.1 }}>
+                <a className="nav-link" href="log-in">
+                  Log in
+                </a>
+              </motion.li>
+              <motion.li className="nav-item" whileHover={{ scale: 1.1 }}>
+                <a className="nav-link" href="sign-up">
+                  Sign up
+                </a>
+              </motion.li>
+            </ul>
+            <button href="#cart" className="btn btn-outline-warning" type="submit" style={{ color: 'grey', fontSize: '20px' }}>
+              🛒Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
