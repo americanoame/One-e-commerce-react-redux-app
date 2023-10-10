8083b4b3b6ed79911afaecc80d0657b8389b3793
Americano.Dev <andrenations@gmail.com>
2023-10-10 17:48:23 -0400

    fixing the container and button

--- frontend/src/screens /ProductsScreen.jsx
+++ frontend/src/screens /ProductsScreen.jsx
@@ -3,8 +3,8 @@ import { products } from '../products';
 
 const ProductsScreen = () => {
   return (
-    <div className="container mx-auto min-h-screen">
-      <Row className="justify-content-center mt-4">
+    <div className="container mx-auto min-h-screen mt-5">
+      <Row className="justify-content-center">
         {products.map((product) => (
           <Col key={product._id} xs={12} sm={6} md={4} lg={3}>
             <Card className="custom-card mb-4 py-4">
@@ -12,7 +12,7 @@ const ProductsScreen = () => {
               <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                <Button variant="primary" type="button"   className='w-100 --btn' 
+                <Button variant="primary" type="button" className='w-100 --btn' 
                 href="/" style={{ color: 'gold', border: 'none' }}>Add to Cart</Button>   
               </Card.Body>
             </Card>
@@ -24,4 +24,7 @@ const ProductsScreen = () => {
 };
 
 export default ProductsScreen;
+
+ 
+
 
