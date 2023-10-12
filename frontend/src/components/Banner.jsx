03b85fad95f8d9e00bac095fdd3db70aa41f84df
Americano.Dev <andrenations@gmail.com>
2023-10-11 22:23:12 -0400

    added carousel

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,39 @@
+const img7 = '/images/slider.jpg';
+const img8 = '/images/slider1.jpg';
+const img9 = '/images/slider2.jpg';
+
+function Banner() {
+  return (
+    
+        <div className="">
+          <div className="container-xxl mb-2 banner-container mb-2 " style={{height: '30vh'}}>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img src={img7} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item ">
+                  <img src={img8} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src={img9} className="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+    
+  );
+}
+
+export default Banner;
+
+
