eabe7950e9096577de0900bfccc1292f800b9043
Americano.Dev <andrenations@gmail.com>
2023-10-11 22:23:58 -0400

    cleaning code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import ProductsScreen from './screens /ProductsScreen';
 import LogIn from './screens /LogIn';
 import SignUp from './screens /SignUp';
 import Navbar from './components/Navbar';
-
 import './App.css'
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/products" element={<ProductsScreen />} />
           <Route path="/log-in" element={<LogIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignUp />} />  
         </Routes>
       
     </BrowserRouter>
