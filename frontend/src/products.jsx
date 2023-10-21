870a096656b2b0b4e63c454f203c8aa1d477f5d2
Americano.Dev <andrenations@gmail.com>
2023-10-21 18:13:03 -0400

    added another line of same product to work and the load more bar

--- frontend/src/products.jsx
+++ frontend/src/products.jsx
@@ -138,4 +138,73 @@ export const products = [
       isNew: true,
       reviews: [],
     },
+
+
+
+  // this the is the exrtra line a added
+
+    {
+      _id: '621235e59540445b3sdfe9s1',
+      name: 'Samsung Galaxy Z Flip4 5G',
+      image: '/images/flip.jpg',
+      description:
+        "Introducing Samsung Galaxy Z Flip4. Fold it, flip it, stand it. Capture photos and videos hands-free with a camera designed to stand on its own with revolutionary folding glass or get that quick selfie when folded for moments that can't wait. ",
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 599.99,
+      rating: 5,
+      numReviews: 1,
+      isNew: true,
+      reviews: [],
+      stock: 7,
+    },
+    {
+      _id: '636985e59540445b3c45e6a2',
+      name: 'iPhone 11 Pro 256GB Memory',
+      image: '/images/iphone.jpg',
+      description:
+        'Shoot 4K video, beautiful portraits and sweeping landscapes with the all-new dual-camera system. Capture your best low-light photos with Night mode. See true-to-life colour in your photos, videos and games on the 6.1-inch Liquid Retina display.3 Experience unprecedented performance with A13 Bionic for gaming, augmented reality and photography',
+      brand: 'Cannon',
+      category: 'Electronics',
+      price: 929.99,
+      rating: 3,
+      numReviews: 1,
+      isNew: false,
+  
+      reviews: [],
+      stock: 5,
+    },
+    {
+      _id: '6369860d9540445b3c45e6b3',
+      name: 'Samsung Galaxy Tab S6',
+      image: '/images/tab.jpg',
+      description:
+        "Galaxy Tab S6 Lite is your super carriable note-taking, go-getting companion. It comes with a large 10.4 inch display on a slim and light build, One UI 4 on Android, and S Pen in box and ready to go. Whether you're drawing, learning or gaming, this is the tablet made to be in the moment. ",
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 11,
+      rating: 5,
+      numReviews: 0,
+      isNew: false,
+  
+      reviews: [],
+      stock: 0,
+    },
+    {
+      _id: '6369863c9540445b3c45e6b4',
+      name: 'Samsung Galaxy Watch5 Pro',
+      image: '/images/s4prowatch.jpg',
+      description:
+        'Meet the Galaxy Watch5. It comes with an innovative 3-in-1 BioActive Sensor designed to accurately monitor and help improve your health, a durable Aluminium finish and a stylish design built for the grind with 60% stronger Sapphire Crystal Glass.',
+      brand: 'Logitech',
+      category: 'Electronics',
+      price: 299.99,
+      rating: 5,
+      numReviews: 0,
+      isNew: false,
+  
+      reviews: [],
+      stock: 7,
+    },
   ];
