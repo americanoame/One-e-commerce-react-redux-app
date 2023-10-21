d7f5303ee77f33aa5c2403dfee7af5f2b1798c7d
Americano.Dev <andrenations@gmail.com>
2023-10-21 18:15:11 -0400

    changed the container

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,48 +1,28 @@
-import { Navbar } from "react-bootstrap";
+import { Navbar } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 
 const AppNavbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary bg-white fixed-top">
-      <div className="container">
-        <Navbar.Brand href="/products" className="mr-auto" 
-          style={{ color: 'grey', fontSize: '23px', fontFamily: 'Times New Roman', fontWeight: 'bolder' }}>
+      <div className="container-xxl">
+        <Navbar.Brand href="/" className="brand" style={{ color: 'grey', fontSize: '25px', fontFamily: 'Times New Roman', fontWeight: 'bolder', marginLeft: '40px' }}>
           AVaLoZe
         </Navbar.Brand>
 
-        <button
-          className="navbar-toggler shadow-none border-0"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div
-          className="offcanvas offcanvas-top"
-          tabIndex="-1"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          style={{ height: '30vh', color: 'black' }}
-        >
+        <div className="offcanvas offcanvas-top" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style={{ height: '50vh', color: 'black' }}>
           <div className="offcanvas-header">
-            <Navbar.Brand href="/products" className="me-5" 
-              style={{ color: 'black', fontSize: '23px', fontFamily: 'Times New Roman', fontWeight: 'bolder' }}>
+            <Navbar.Brand href="/products" className="me-5" style={{ color: 'black', fontSize: '23px', fontFamily: 'Times New Roman', fontWeight: 'bolder' }}>
               AVaLoZe
             </Navbar.Brand>
-            <button
-              type="button"
-              className="btn-close shadow-none border-0"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
+            <button type="button" className="btn-close shadow-none border-0" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-end flex-grow-1">
               <motion.li className="nav-item" whileHover={{ scale: 1.2 }}>
-                <a className="nav-link active" aria-current="page" href="/products">
+                <a className="nav-link active" aria-current="page" href="/">
                   Products
                 </a>
               </motion.li>
@@ -57,10 +37,11 @@ const AppNavbar = () => {
                 </a>
               </motion.li>
             </ul>
-            <a href="" className="btn btn-outline-warning" type="button" 
-              style={{ color: 'grey', fontSize: '20px' }}>
-              🛒Cart
-            </a>
+            <button className="btn btn-white shadow-sm rounded-pill " 
+            style={{ fontSize: '12px', backgroundColor: 'gold', 
+            color: 'grey', border: 'none', marginRight: '40px'}}>
+              🛒 Cart
+            </button>
           </div>
         </div>
       </div>
