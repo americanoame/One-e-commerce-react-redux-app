d33c8fd03e1aa688c0fffa25c451b27598ce0e0b
Americano.Dev <andrenations@gmail.com>
2023-10-27 00:11:09 -0400

    added navigation

--- frontend/src/screens /LogIn.jsx
+++ frontend/src/screens /LogIn.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 import { Form, Button, Container, Col } from 'react-bootstrap';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function LogIn() {
-
   const [formData, setFormData] = useState({});
-  const {loading, error} = useSelector((state) => state.user)
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -28,13 +27,13 @@ export default function LogIn() {
         },
         body: JSON.stringify(formData),
       });
-      const data = await res.json(); 
+      const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailure(data));
         return;
       }
-       
-      // "If data.success is equal to false, then setError(true) 
+
+      // "If data.success is equal to false, then setError(true)
       // is called. Otherwise, do nothing (null)."
       dispatch(signInSuccess(data));
       navigate('/products');
@@ -42,64 +41,44 @@ export default function LogIn() {
       dispatch(signInFailure(error));
     }
   };
-  
-    return (
-      <Container className="d-flex justify-content-center align-items-center mt-5">
-        <div className="bg-white p-4">
-        <h2 className="text-2xl text-center font-semibold mb-4" style={{color: 'grey', fontWeight: '600'}}>Log In</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="email">
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center mt-5">
+      <div className="bg-white p-4">
+        <h2 className="text-2xl text-center font-semibold mb-4" style={{ color: 'grey', fontWeight: '600' }}>
+          Log In
+        </h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="email">
             <Col xs={2}>
               <Form.Label className="mt-2">Email</Form.Label>
             </Col>
-              
-              <Form.Control
-                type="email"
-                name="email"
-                
-                className="w-full px-5 py-2 border rounded-lg"
-                onChange={handleChange}
-              />
-            </Form.Group>
-  
-            <Form.Group controlId="password">
-             <Col xs={2}>
-              <Form.Label className="mt-2" >Password</Form.Label>
+
+            <Form.Control type="email" name="email" className="w-full px-5 py-2 border rounded-lg" onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Col xs={2}>
+              <Form.Label className="mt-2">Password</Form.Label>
             </Col>
-              <Form.Control
-                type="password"
-                name="password"
-                
-                className="w-full px-5 py-2 border rounded-lg"
-               onChange={handleChange}
-              />
-            </Form.Group>
-  
-            <Button disabled={loading} variant="primary" type="submit"   className='w-100 --btn mt-3' 
-                 style={{ color: 'grey', border: 'none', textTransform: 'uppercase', fontWeight: 'bold' }}>
-                 {loading ? 'Loading...' : 'Log In'}
-                 </Button>
-          </Form>
-  
-          <p className="text-gray-600 text-sm mt-4">
-            Have no account?{' '}
-            <a href="/sign-up" className="text-green-500" style={{color: 'green' }}>
-              Sign Up 
-            </a>
-          </p>
-          <p className={`text-danger `}> {error ? error.message || 'Something went wrong!' : ''} </p>
-        </div>
-      </Container>
-    );
-  }
+            <Form.Control type="password" name="password" className="w-full px-5 py-2 border rounded-lg" onChange={handleChange} />
+          </Form.Group>
 
-  
+          <Link>
+            <Button disabled={loading} variant="primary" type="submit" className="w-100 --btn mt-3" style={{ color: 'grey', border: 'none', textTransform: 'uppercase', fontWeight: 'bold' }}>
+              {loading ? 'Loading...' : 'Log In'}
+            </Button>
+          </Link>
+        </Form>
 
-
-
-
-
-  
-
-
-
+        <p className="text-gray-600 text-sm mt-4">
+          Have no account?{' '}
+          <a href="/sign-up" className="text-green-500" style={{ color: 'green' }}>
+            Sign Up
+          </a>
+        </p>
+        <p className={`text-danger `}> {error ? error.message || 'Something went wrong!' : ''} </p>
+      </div>
+    </Container>
+  );
+}
--- frontend/src/screens /SignUp.jsx
+++ frontend/src/screens /SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { Form, Button, Container, Col } from 'react-bootstrap';
 
@@ -9,7 +9,7 @@ export default function SignUp() {
 
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -31,8 +31,8 @@ export default function SignUp() {
       console.log(data);
       setLoading(false);
       data.success === false ? setError(true) : null;
-       
-      // "If data.success is equal to false, then setError(true) 
+
+      // "If data.success is equal to false, then setError(true)
       // is called. Otherwise, do nothing (null)."
 
       navigate('/log-in');
@@ -42,77 +42,50 @@ export default function SignUp() {
     }
   };
 
+  return (
+    <Container className="d-flex justify-content-center align-items-center mt-5">
+      <div className="bg-white p-4">
+        <h2 className="text-2xl text-center font-semibold mb-4" style={{ color: 'grey', fontWeight: '600' }}>
+          Sign Up
+        </h2>
 
-    return (
-      <Container className="d-flex justify-content-center align-items-center mt-5">
-        <div className="bg-white p-4">
-        <h2 className="text-2xl text-center font-semibold mb-4" style={{color: 'grey', fontWeight: '600'}}>Sign Up</h2>
-  
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="username">
             <Col xs={3}>
               <Form.Label>Username</Form.Label>
             </Col>
-              <Form.Control
-                type="text"
-                name="username"
-                
-                className="w-full px-5 py-2 border rounded-lg"
-                onChange={handleChange}
-              />
-            </Form.Group>
-  
-            <Form.Group controlId="email">
+            <Form.Control type="text" name="username" className="w-full px-5 py-2 border rounded-lg" onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="email">
             <Col xs={2}>
               <Form.Label className="mt-2">Email</Form.Label>
             </Col>
-              
-              <Form.Control
-                type="email"
-                name="email"
-                
-                className="w-full px-5 py-2 border rounded-lg"
-                onChange={handleChange}
-              />
-            </Form.Group>
-  
-            <Form.Group controlId="password">
-             <Col xs={2}>
-              <Form.Label className="mt-2" >Password</Form.Label>
+
+            <Form.Control type="email" name="email" className="w-full px-5 py-2 border rounded-lg" onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Col xs={2}>
+              <Form.Label className="mt-2">Password</Form.Label>
             </Col>
-              <Form.Control
-                type="password"
-                name="password"
-                
-                className="w-full px-5 py-2 border rounded-lg"
-               onChange={handleChange}
-              />
-            </Form.Group>
-  
-            <Button disabled={loading} variant="primary" type="submit"   className='w-100 --btn mt-3' 
-                 style={{ color: 'grey', border: 'none', fontWeight: 'bold', textTransform: 'uppercase'}}>
-                 {loading ? 'Loading...' : 'Sign Up'}
-                 </Button>
-          </Form>
-  
-          <p className="text-gray-600 text-sm mt-4">
-            Have an account?{' '}
-            <a href="/log-in" className="text-green-500" style={{color: 'green'}}>
-              Log in
-            </a>
-          </p>
-          <p className={`text-danger `}> {error && 'Something went wrong!'} </p>
-        </div>
-      </Container>
-    );
-  }
+            <Form.Control type="password" name="password" className="w-full px-5 py-2 border rounded-lg" onChange={handleChange} />
+          </Form.Group>
 
+           
+          <Button disabled={loading} variant="primary" type="submit" className="w-100 --btn mt-3" style={{ color: 'grey', border: 'none', fontWeight: 'bold', textTransform: 'uppercase' }}>
+            {loading ? 'Loading...' : 'Sign Up'}
+          </Button>
+        </Form>
 
-
-
-
-
-
-  
-
-
+        <p className="text-gray-600 text-sm mt-4">
+          Have an account?{' '}
+          <a href="/log-in" className="text-green-500" style={{ color: 'green' }}>
+            Log in
+          </a>
+        </p>
+        <p className={`text-danger `}> {error && 'Something went wrong!'} </p>
+      </div>
+    </Container>
+  );
+}
